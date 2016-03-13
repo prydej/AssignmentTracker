3486949dfdad1c497e730c259457bf0b288bf15a
Nuly90 <kochj6@my.erau.edu>
2016-03-13 10:05:57 -0400

    "Account for leap years and time zones"
    - Add conditions to account for leap years
    - Add conditions to account for time zone differences in the DTG string

--- AssignmentStatus.py
+++ AssignmentStatus.py
@@ -8,6 +8,7 @@ purpose: give information on current assignments
 """
 import time
 import json
+import string
 from Colors import *
 
 
@@ -16,25 +17,95 @@ def time_diff(time2):
     """Takes two times in DTG and returns the time difference between them"""
 
     # Create DTG for current time
-    time1 = time.strftime('%d%H%MR%b%y')
+    time1 = time.strftime('%d%H%MZ%b%y', time.gmtime())
 
     # Casefold times
     time1 = time1.casefold()
     time2 = time2.casefold()
 
+    # Determine if Daylight Savings Time is active
+    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun','jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    mind = months.index(time2[7:10])
+    day = int(time2[0:2])
+    dst = (mind > 2 or (mind == 2 and day >= 13)) and (mind < 10 or (mind == 10 and day <= 6))
+
+    # Convert provided time to Z
+    alphabet = list(string.ascii_lowercase)
+    alphabet.remove('j')
+    zone2s = time2[6]
+    zone2 = alphabet.index(zone2s)
+    if zone2 < alphabet.index('n'):
+        convert = zone2 + 1
+        print(convert)
+
+    elif zone2s == 'z':
+        convert = 0
+        print(convert)
+
+    else:
+        convert = alphabet.index('n') - zone2 - 1
+        if dst:
+            convert += 1
+        print(convert)
+
+    hour2 = int(time2[2:4])
+    hour2 += convert
+
+    hdef = True
+    ddef = True
+    mdef = True
+    ydef = True
+
+    if hour2 < 24:
+        hdef = False
+
+    else:
+        day2 = int(time2[0:2]) + 1
+        hour2 -= 24
+        hdef = False
+
+        month2 = months.index(time2[7:10])
+
+        if day2 <= mdays[month2] and day2 >= 10:
+            ddef = False
+
+        else:
+            month2 += 1
+            ddef = False
+
+            if month2 <= 12:
+                mdef = False
+
+            else:
+                year2 = int(time2[10:12]) + 1
+                month2 = 1
+                mdef = False
+
+                if year2 >= 10:
+                    ydef = False
+
     # Determine year difference
     year1 = int(time1[10]) * 10 + int(time1[11])
-    year2 = int(time2[10]) * 10 + int(time2[11])
+
+    if ydef:
+        year2 = int(time2[10]) * 10 + int(time2[11])
+
     ydiff = year2 - year1
     yddiff = ydiff * 365
 
     # Determine month difference
-    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun','jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-    mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for i in range(ydiff + 1):
+        if (year1 + i) % 4 == 0:
+            mdays[1] += 1
+
     month1 = time1[7:10]
     month1 = months.index(month1)
-    month2 = time2[7:10]
-    month2 = months.index(month2)
+
+    if mdef:
+        month2 = time2[7:10]
+        month2 = months.index(month2)
+
     mdiff = 0
     for i in range(month1, month2 - 1):
         mdiff += mdays[i]
@@ -42,16 +113,22 @@ def time_diff(time2):
     # Determine day difference
     day1 = time1[0:2]
     day1 = int(day1)
-    day2 = time2[0:2]
-    day2 = int(day2)
+
+    if ddef:
+        day2 = time2[0:2]
+        day2 = int(day2)
+
     ddiff = day2 - day1
     tddiff = ddiff + mdiff + yddiff
 
     # Determine hour difference
     hour1 = time1[2:4]
     hour1 = int(hour1)
-    hour2 = time2[2:4]
-    hour2 = int(hour2)
+
+    if hdef:
+        hour2 = time2[2:4]
+        hour2 = int(hour2)
+
     hdiff = hour2 - hour1
     thdiff = hdiff + tddiff * 24
 
