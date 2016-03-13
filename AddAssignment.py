a4f107b3a5ebbaa689c6563ecfad7dbcb20c9abd
Julian Pryde <prydej@my.erau.edu>
2016-03-12 20:27:29 -0500

    "Add ability to write JSON object with new assignment to file"  - not yet in array  - does not yet store nextid

--- AddAssignment.py
+++ AddAssignment.py
@@ -2,13 +2,13 @@
 Author: Julian Pryde
 File: AddAssignment.py
 Name: Add Assignment
+Project: Homework Planner
 Purpose: Write new assignment to file for Assignment Tracker
 """
 import json
 
 
-def add_assignment():
-
+def add_assignment(all_asses, nextid):
     # Ask user for information about new ass
     ass_name = input('Name of assignment: ')
     ass_date = input('Due date: ')
@@ -16,19 +16,27 @@ def add_assignment():
     ass_priority = input('Priority: ')
     ass_course = input('Class name: ')
 
-    # # Create object of class Ass
-    # ass = Ass(ass_name, ass_date, ass_time, ass_priority, ass_class)
-    #
-    # # Create dictionary
-    # ass = dict(ass)
+    # Assign ID Number
+    ass_id = nextid
+    nextid += 1
 
-    ass_dict = {'name': ass_name, 'date': ass_date, 'time': ass_time, 'priority': ass_priority,
-                'course': ass_course}
+    # Create dict to write to file
+    ass_dict = {'name': ass_name, 'date': ass_date, 'time': ass_time, 'priority': ass_priority, 'course': ass_course,
+                'ID': ass_id}
 
-    print("ass_dict['name']", ass_dict['name'])
+    # Create json string from object
+    new_ass_string = json.dumps(ass_dict, sort_keys=True, indent=4, separators=(',', ': '))
+    print(new_ass_string)
+
+    # Concatenate current dict with previous assignments
+    all_asses.append(ass_dict)
+
+    # Update nextid in new assignment
+    print(type(ass_dict))
+    ass_dict[0] = {"nextid": 0}
 
     # Write object to file
-    with open('Asshole.json', 'a', 1) as asshole:
-        json.dump(ass_dict, asshole)
+    with open('Asshole.json', 'w', 1) as asshole:
+        json.dump(new_ass_string, asshole)
 
     return None
--- HW_Planner.py
+++ HW_Planner.py
@@ -5,6 +5,24 @@
 # Provide and process main menu
 
 from AddAssignment import *
+import os
+
+# Get last sequential ID number
+with open('Asshole.json', 'w+', 1) as asshole:
+
+    # If new file is empty
+    if os.stat("Asshole.json").st_size:
+        all_asses = json.loads(asshole.read())
+    else:
+        asshole.write("[]")
+        all_asses = []
+
+    # If new file, add array with only nextid
+    if "nextid" in all_asses:
+        nextid = all_asses[0]["nextid"]
+    else:
+        asshole.write('[{"nextid": 0}]')
+        nextid = 0
 
 if __name__ == '__main__':
 
@@ -13,12 +31,12 @@ if __name__ == '__main__':
     while choice != 'x':
 
         # Print menu
-        print('What do you want to do?\n')
-        print('a. Add assignment\n')
-        print('u. Update assignment\n')
-        print('d. Display status\n')
-        print('c. Mark assignment as complete\n')
-        print('x. Exit the program\n')
+        print('What do you want to do?')
+        print('a. Add assignment')
+        print('u. Update assignment')
+        print('d. Display status')
+        print('c. Mark assignment as complete')
+        print('x. Exit the program')
 
         # Take input
         choice = input()
@@ -26,7 +44,7 @@ if __name__ == '__main__':
 
     # Call appropriate function
         if choice == 'a':
-            add_assignment()
+            add_assignment(all_asses, nextid)
 
         elif choice == 'u':
             print('You chose u')
@@ -40,17 +58,7 @@ if __name__ == '__main__':
         elif choice != 'x':
             print("You're an idiot.")
 
-
-# Display status
-
-
-# Mark assignment as complete
-
-
-# Create new assignment
-
-
-# Update assignment
+#load
 
 class Ass(object):
 
